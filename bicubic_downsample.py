6f813a9b214878d1eaa18adfdaec6188b1703a8c
charleslucky <1214176716qq>
2021-09-02 19:45:57 +0800

    xingbo add resize

---
+++ bicubic_downsample.py
@@ -0,0 +1,61 @@
+# TensorFlow Better Bicubic Downsample
+import tensorflow as tf
+import numpy as np
+
+
+def bicubic_kernel(x, a=-0.5):
+  """https://clouard.users.greyc.fr/Pantheon/experiments/rescaling/index-en.html#bicubic"""
+  if abs(x) <= 1:
+    return (a + 2)*abs(x)**3 - (a + 3)*abs(x)**2 + 1
+  elif 1 < abs(x) and abs(x) < 2:
+    return a*abs(x)**3 - 5*a*abs(x)**2 + 8*a*abs(x) - 4*a
+  else:
+    return 0
+
+def build_filter(factor):
+  size = factor*4
+  k = np.zeros((size))
+  for i in range(size):
+    x = (1/factor)*(i- np.floor(size/2) +0.5)
+    k[i] = bicubic_kernel(x)
+  k = k / np.sum(k)
+  # make 2d
+  k = np.outer(k, k.T)
+  k = tf.constant(k, dtype=tf.float32, shape=(size, size, 1, 1))
+  return tf.concat([k, k, k], axis=2)
+
+def apply_bicubic_downsample(x, filter, factor):
+  """Downsample x by a factor of factor, using the filter built by build_filter()
+  x: a rank 4 tensor with format NHWC
+  filter: from build_filter(factor)
+  factor: downsampling factor (ex: factor=2 means the output size is (h/2, w/2))
+  """
+  # using padding calculations from https://www.tensorflow.org/api_guides/python/nn#Convolution
+  filter_height = factor*4
+  filter_width = factor*4
+  strides = factor
+  pad_along_height = max(filter_height - strides, 0)
+  pad_along_width = max(filter_width - strides, 0)
+  # compute actual padding values for each side
+  pad_top = pad_along_height // 2
+  pad_bottom = pad_along_height - pad_top
+  pad_left = pad_along_width // 2
+  pad_right = pad_along_width - pad_left
+  # apply mirror padding
+  x = tf.pad(x, [[0,0], [pad_top,pad_bottom], [pad_left,pad_right], [0,0]], mode='REFLECT')
+  # downsampling performed by strided conv
+  x = tf.nn.depthwise_conv2d(x, filter=filter, strides=[1,strides,strides,1], padding='VALID')
+  return x
+
+
+
+# from bicubic_downsample import build_filter, apply_bicubic_downsample
+
+# First, create the bicubic kernel. This can be reused in multiple downsample operations
+k = build_filter(factor=2)
+x = tf.Variable(np.random.normal(size=(1,100,100,3)), dtype=tf.float32)
+print(x.shape)
+# Downsample x which is a tensor with shape [N, H, W, 3]
+y = apply_bicubic_downsample(x, filter=k, factor=2)
+print(y)
+# y now contains x downsampled to [N, H/4, W/4, 3]
