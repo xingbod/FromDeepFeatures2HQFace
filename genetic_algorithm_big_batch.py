218d0ba4b20558ab2c76cfd6064202ac1fbbb9a4
charleslucky <xingbod@gmail.com>
2021-09-09 16:35:11 +0800

    xingbo

--- genetic_algorithm_big_batch.py
+++ genetic_algorithm_big_batch.py
@@ -88,8 +88,8 @@ def GAalgo(population,crossover_mat_ph,mutation_val_ph):
     feature_new = np.zeros(pop_size,512)
     image_out = np.zeros(pop_size,1024,1024,3)
     for batch in range(big_batch_size):
-        input = population[batch:batch*32,:]# tf.Variable(np.random.randn(32, features), dtype=tf.float32)
-        feature_new[batch:batch*32,:], image_out[batch:batch*32,:,:,:] = model(input)
+        input = population[batch:(batch+1)*32,:]# tf.Variable(np.random.randn(32, features), dtype=tf.float32)
+        feature_new[batch:(batch+1)*32,:], image_out[batch:(batch+1)*32,:,:,:] = model(input)
 
     # print("xxxx*******2")
     fitness = -tf.reduce_mean(tf.square(tf.subtract(feature_new, truth_ph)), 1)
