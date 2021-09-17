119951abb084b7113f59946b1716d54128abab47
mzhmia <mzhmia@qq.com>
2021-09-17 17:38:26 +0800

    09.17 v2

--- create_feature.py
+++ create_feature.py
@@ -4,45 +4,63 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.python.framework import ops
 from shutil import copy
-from ModelZoo import loadArcfaceModel
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+# os.environ['TF_CPP_MIN_LOG_LEVEL']='3'# 只显示 Error
+import logging
+logging.disable(30)# for disable the warnning in gradient tape
+
+from ModelZoo import loadStyleGAN2Model, loadArcfaceModel
+
 from skimage import io
-from skimage.transform import rescale, resize, downscale_local_mean
+from skimage.transform import resize
 import pickle
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
-# lfw_dataset_path = r"D:\data\lfw_select"
+# lfw_dataset_path = r"D:\data\celeba_select"
 # names_list = os.listdir(lfw_dataset_path)
 # print(names_list)
-
+#
 # for name in names_list:
-#     imgPair_path = os.path.join(r"D:\data\lfw_pairs", f"{name}_pair")
-#     imgResult_path = os.path.join(r"D:\data\lfw_select_result", f"{name}")
+#     imgPair_path = os.path.join(r"D:\data\celeba_pairs", f"{name}_pair")
+#     imgResult_path = os.path.join(r"D:\data\celeba_select_result", f"{name}")
 #     if not os.path.exists(imgPair_path):
 #         os.mkdir(imgPair_path)
+#     # 保存原图
 #     source_img_path = os.path.join(imgPair_path, "source_img")
 #     if not os.path.exists(source_img_path):
 #         os.mkdir(source_img_path)
 #         imgs_list = os.listdir(os.path.join(lfw_dataset_path, name))
 #         for img in imgs_list:
 #             copy(os.path.join(os.path.join(lfw_dataset_path, name), img), source_img_path)
+#     # 保存ground truth
 #     img_result_list = os.listdir(imgResult_path)
 #     gt_img = [idx for idx in img_result_list if idx[0:2] == 'gt']
 #     gt_img_path = os.path.join(imgPair_path, "gt_img")
 #     if not os.path.exists(gt_img_path):
 #         os.mkdir(gt_img_path)
 #         copy(os.path.join(imgResult_path, gt_img[0]), gt_img_path)
-#     os.mkdir(os.path.join(imgPair_path, "pred_img"))
+#    # 保存预测图
+#     pred_img_path = os.path.join(imgPair_path, "pred_img")
+#     if not os.path.exists(pred_img_path):
+#         os.mkdir(pred_img_path)
+#         out_imgs = [idx for idx in img_result_list if idx[0:3] == 'out']
+#         loss_array = list()
+#         for out_imgs_name in out_imgs:
+#             (filename, extension) = os.path.splitext(out_imgs_name)
+#             name_split = filename.split('_', )
+#             loss_array.append(float(name_split[-1]))
+#         index = int(np.argmin(loss_array))
+#         copy(os.path.join(imgResult_path, out_imgs[index]), pred_img_path)
 
 
 
 
 arcfacemodel = loadArcfaceModel()
 
-lfw_pairs_path = "./data/lfw_pairs"
+lfw_pairs_path = "./data/celeba_pairs"
 names_list = os.listdir(lfw_pairs_path)
 
-lfw_feature_path = "./data/lfw_feature_pairs"
+lfw_feature_path = "./data/celeba_feature_pairs"
 
 for name in names_list:
     img_path = os.path.join(lfw_pairs_path, name)
@@ -94,7 +112,7 @@ for name in names_list:
             img = resize(img, (112, 112), anti_aliasing=True)
             img = np.array(img)
             img = np.expand_dims(img, 0)
-            source_feature.append(arcfacemodel(img).numpy)
+            source_feature.append(arcfacemodel(img).numpy())
 
         file_source = open(f"{source_feature_path}/source.pickle", "wb")
         pickle.dump(source_feature, file_source)
---
+++ draw_graph.py
@@ -0,0 +1,101 @@
+import os
+import numpy as np
+import random
+import pickle
+import matplotlib.pyplot as plt
+import itertools
+import cv2
+import seaborn as sns
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+os.environ['TF_CPP_MIN_LOG_LEVEL']='3'# 只显示 Error
+
+def euc_sim(a, b):
+    return 1 - np.linalg.norm(a - b) / (np.linalg.norm(a) + np.linalg.norm(b))
+
+
+features_path = "./data/celeba_feature_pairs"
+
+names_list = os.listdir(features_path)
+
+
+# features_list = list()
+def nchoosek(startnum, endnum, step=1, n=1):
+    c = []
+    for i in itertools.combinations(range(startnum,endnum+1,step),n):
+        c.append(list(i))
+    return c
+
+
+# calculate imposter scores
+def cal_imposter_scorces():
+    imposter_scores = []
+    for i,j in nchoosek(0, 58, step=1, n=2):
+        with open(os.path.join(features_path, names_list[i], 'source_feature', 'source.pickle'), 'rb') as file:
+            features1 = np.array(pickle.load(file))[:3]
+        with open(os.path.join(features_path, names_list[j], 'source_feature', 'source.pickle'), 'rb') as file:
+            features2 = np.array(pickle.load(file))[:3]
+        imposter_scores.append(euc_sim(features1 , features2))
+    return imposter_scores
+
+
+def cal_genuine_scorces():
+    genuine_scores = []
+    for name in names_list:
+        with open(os.path.join(features_path, name, 'source_feature', 'source.pickle'), 'rb') as file:
+            features_list = np.array(pickle.load(file))
+        for i, j in nchoosek(0, len(features_list)-1, step=1, n=2):
+            feature1 = features_list[i]
+
+            feature2 = features_list[j]
+            genuine_scores.append(euc_sim(feature1, feature2))
+
+    return genuine_scores
+
+
+def cal_attack_scorces():
+    attack_scores = []
+    for name in names_list:
+        with open(os.path.join(features_path, name, 'gt_feature', 'gt.pickle'), 'rb') as gt_file:
+            gt_feature = np.array(pickle.load(gt_file))
+        with open(os.path.join(features_path, name, 'pred_feature', 'pred.pickle'), 'rb') as pred_file:
+            pred_feature = np.array(pickle.load(pred_file))
+        attack_scores.append(euc_sim(gt_feature, pred_feature))
+    return attack_scores
+
+
+def cal_attack_scorces2():
+    attack_scores2 = []
+    for name in names_list:
+        with open(os.path.join(features_path, name, 'pred_feature', 'pred.pickle'), 'rb') as pred_file:
+            pred_feature = np.array(pickle.load(pred_file))
+        with open(os.path.join(features_path, name, 'source_feature', 'source.pickle'), 'rb') as file:
+            features_list = np.array(pickle.load(file))
+        for i in range(len(features_list)):
+            attack_scores2.append(euc_sim(pred_feature, features_list[i]))
+    return attack_scores2
+
+
+def draw_pic1():
+    sns.set_style("white")
+    kwargs = dict(kde_kws={'linewidth': 0.001})
+    plt.figure(figsize=(10, 7), dpi=80)
+    sns.distplot(cal_genuine_scorces(), color="dodgerblue", label="Geniue score", **kwargs)
+    sns.distplot(cal_imposter_scorces(), color="orange", label="Imposter score", **kwargs)
+    sns.distplot(cal_attack_scorces(), color="deeppink", label="Reconstructed face score", **kwargs)
+    plt.legend()
+    plt.savefig("score_dist_celeba_type1.svg")
+
+def draw_pic2():
+    sns.set_style("white")
+    kwargs = dict(kde_kws={'linewidth': 0.001})
+    plt.figure(figsize=(10, 7), dpi=80)
+    sns.distplot(cal_genuine_scorces(), color="dodgerblue", label="Geniue score", **kwargs)
+    sns.distplot(cal_imposter_scorces(), color="orange", label="Imposter score", **kwargs)
+    sns.distplot(cal_attack_scorces2(), color="deeppink", label="Reconstructed face score", **kwargs)
+    plt.legend()
+    plt.savefig("score_dist_celeba_type2.svg")
+
+if __name__ == '__main__':
+    draw_pic1()
+    draw_pic2()
